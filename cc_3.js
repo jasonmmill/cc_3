ad7781a31e083be89d9f613c6532f753a15000df
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-02 18:49:43 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -4,3 +4,11 @@ prices.push(120)
 // console.log(prices)
 prices.splice(0,1)
 console.log(prices)
+
+// Task 2 - Modifying Customer Orders Scenario
+
+let orders = [ 5, 10, 15, 20, 25 ]
+orders[2] += 5
+console.log(orders)
+orderTotal = orders.reduce((total, num) => total + num, 0)
+console.log(orderTotal)
