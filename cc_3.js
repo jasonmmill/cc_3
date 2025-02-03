a8bd769debf3bd101e8bd5733b4bc25f5361b900
Jasonmmill <jasonmatthewmill@gmail.com>
2025-02-02 19:30:42 -0500

    Task 5 - Inventory Management System

--- cc_3.js
+++ cc_3.js
@@ -3,15 +3,15 @@ let prices = [ 95, 100, 105, 110, 115 ]
 prices.push(120)
 // console.log(prices)
 prices.splice(0,1)
-console.log(prices)
+console.log("Prices:", prices)
 
 // Task 2 - Modifying Customer Orders Scenario
 
 let orders = [ 5, 10, 15, 20, 25 ]
 orders[2] += 5
-console.log(orders)
+console.log("Orders:", orders)
 orderTotal = orders.reduce((total, num) => total + num, 0)
-console.log(orderTotal)
+console.log("Order Total:", orderTotal)
 
 // Task 3 - Employee Performance Tracking Scenario
 
@@ -23,7 +23,7 @@ let employee = {
 }
 employee.performanceScore = 85
 employee.promotionEligible = false
-console.log(employee)
+console.log("Employee:", employee)
 
 // Task 4 - Customer Feedback Records Scenario
 
@@ -33,4 +33,17 @@ let feedback = [
     {employeeName: "Joshua Alderman", feedbackText: "Valued Customer", rating: 70}
 ]
 feedback.push({employeeName: "Juliana Joseph", feedbackText: "Unvalued Customer", rating: 35})
-console.log(feedback)
+console.log("Feedback: ", feedback)
+
+// Task 5 - Inventory Management System Scenario
+
+let inventory = {
+    itemName: "Macbook Air",
+    stockCount: 550,
+    price: 1000,
+    calculateTotalValue: function() {
+        return this.stockCount * this.price
+    }
+}
+console.log("Inventory: ",inventory)
+console.log("Total Value:", inventory.calculateTotalValue())
